5fa8418fb03677d93cda7f4d742d15746b680187
rus.ggs <rushikeshgharkar31@gmail.com>
2025-12-08 19:44:28 +0530

    Custom Hooks

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,47 +6,35 @@ import Login from './Components/Login'
 import UseRef from './Components/UseRef'
 import Rollup from './Components/RollUp'
 import Count from './Components/Count'
+import { useState } from 'react'
+
+function useCounter(){
+  const [count , setCount] = useState(0);
+  return {
+    count : count,
+    setCount : setCount
+  }
+}
+
+function Counter(){
+  const {count , setCount} = useCounter();
+  function handleCount(){
+    return setCount(count+1)
+  }
+  return <div>
+    {count}
+    <button onClick={handleCount}>Increase Count</button>
+  </div>
+}
 
 function App() {
 
   return (
     <>
-      {/* <BrowserRouter>
-      <Layout/>
-      <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/allen' element={<Allen />} />
-      </Routes>
-      <Footer/>
-      </BrowserRouter> */}
-      {/* <Login/> */}
-        {/* <UseRef/> */}
-        {/* <Rollup/> */}
-        <Count/>
+     <Counter/>
     </>
   )
 }
 
-function Layout(){
-   return <div>
-          <Header/>
-          <div>
-            <Outlet/>
-          </div>
-          <Footer/>
-   </div>
-}
 
-function Header(){
-  return <div>
-    <Link to="/">Landing</Link>
-    <Link to="/allen">Allen</Link>
-  </div>
-}
-
-function Footer(){
-  return <div>
-    <h1>All rights reserved</h1>
-  </div>
-}
 export default App
